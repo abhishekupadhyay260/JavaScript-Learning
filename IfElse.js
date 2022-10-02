766525bba65265f037aa20b4cfd535b9505f72c1
abhishekupadhyay260 <abhishekupadhyay260@gmail.com>
2022-10-02 17:23:08 +0530

    added if else part

---
+++ IfElse.js
@@ -0,0 +1,9 @@
+var iLikeChocolate = true
+
+if (iLikeChocolate){
+    document.write("yes i like chocolate")
+}
+
+if (29 > 27){
+    document.write("Sahi to bola 29 > 27")
+}
--- index2.js
+++ index2.js
@@ -1,2 +1,8 @@
 alert("leave this space");
-Alert("leave this space"); //js is case sensitive hence this doesnt work
+//Alert("leave this space"); //js is case sensitive hence this doesnt work
+//js follow top to bottom approach so write statement according ly.
+//what you want to do, write it first, then 2nd task in second statement and so on
+// we describe variable in java with "var" not "int, char float etc"
+// for ex- var myName=abhi;
+//examle 2 - var myAge=5;
+//basic mathematical operators also work here, like "+,-,*.%,+=.-+,++,--, boolean etc" 
